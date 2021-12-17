bf21ee7e280504cb1f35e8e42b304e4aaae5d314
root <root@4cd264277083>
2021-12-17 01:26:11 +0000

    SeaCreature

---
+++ src/_01_sea_creature/SeaCreatureRunner.java
@@ -0,0 +1,13 @@
+package _01_sea_creature;
+
+public class SeaCreatureRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SeaCreature spongebob = new SeaCreature("Spongebob");
+		spongebob.eat();
+		spongebob.laugh();
+		
+	}
+
+}
